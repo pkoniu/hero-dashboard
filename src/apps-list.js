2d29c23171067184bf6992998bd12197d51558b6
patrykel <patryk.lawski@gmail.com>
2016-05-18 21:14:25 +0200

    improve spelling in apps-list

--- src/apps-list.js
+++ src/apps-list.js
@@ -7,7 +7,7 @@ module.exports = function () {
     return {
         getList: (req, res) => {
             var list = cache.getAll();
-            if(typeof list === 'undefind'){
+            if(typeof list === 'undefined'){
             	list = heroku.getAllApps();
             }
             res.status(200).json(list);
